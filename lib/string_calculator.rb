8dcd96238ead64917a46b04fdefc7b2f55426211
nitin <nitinkv50@gmail.com>
2024-05-28 23:07:37 +0530

    Added add method for string calculator class

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,4 +1,9 @@
 # lib/string_calculator.rb
 # This is the class where we will create methods to add given strings
 class StringCalculator
+  def add(input)
+    # First split the string by commas
+    # then convert each part to an integer and then sum them up
+    input.split(',').map(&:to_i).sum
+  end
 end
