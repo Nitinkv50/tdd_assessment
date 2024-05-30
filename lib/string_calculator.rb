bf47277f1b659dcfca211a2ad7aee436e4761882
nitin <nitinkv50@gmail.com>
2024-05-30 19:06:51 +0530

    updated code to handles new lines between numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,8 +2,8 @@
 # This is the class where we will create methods to add given strings
 class StringCalculator
   def add(input)
-    # First split the string by commas
-    # then convert each part to an integer and then sum them up
-    input.split(',').map(&:to_i).sum
+    # First split the string by commas and newlines
+    # convert each part to an integer, and sum them up
+    input.split(/,|\n/).map(&:to_i).sum
   end
 end
